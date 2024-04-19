1c65fcccddcb570484b15d7f49a684787eea1cc6
Matheus Carvalho <matheus.dev23@gmail.com>
2024-04-18 22:18:17 -0300

    Listagem de Arquivos

--- src/components/Form/FileInput/Control.tsx
+++ src/components/Form/FileInput/Control.tsx
@@ -5,7 +5,7 @@ import { useFileInput } from "./Root";
 
 export type ControlProps = ComponentProps<'input'>
 
-export function Control(props: ControlProps) {
+export function Control({multiple = false, ...props}: ControlProps) {
   const { id, onFilesSelected } = useFileInput()
 
   function handleFilesSelected(event: ChangeEvent<HTMLInputElement>) {
@@ -15,10 +15,10 @@ export function Control(props: ControlProps) {
 
     const files = Array.from(event.target.files)
 
-    onFilesSelected(files)
+    onFilesSelected(files, multiple)
   }
 
   return(
-    <input type="file" className="sr-only" id={id} onChange={handleFilesSelected} {...props}/>
+    <input type="file" className="sr-only" id={id} onChange={handleFilesSelected} multiple={multiple} {...props}/>
   )
 }
